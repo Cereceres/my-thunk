d3437ceb120bdc31eb6ff6918788eedfa646e79d
cereceres <jesus.edelcereceres@gmail.com>
2018-01-05 21:16:04 -0600

    the test are fixed

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-'use strict'
-
 /**
 * private api
 */
 const slice = Array.prototype.slice;
+
 /**
 * public api
 * build a thunk
 * @function
-* @param {function} asyncFunction, is not neccesary to be async, only receive a
+* @param {function} asyncFunction, is not necessary to be async, only receive a
 * a callback
 * @param {*} params to asyncFunction
 * @return {function} thunk
@@ -23,46 +22,27 @@ const slice = Array.prototype.slice;
 * @return {*}
 */
 
-/**
- * This callback is passed to asyncFunction
- * @callback callback
- * @param {err} error passed by asyncFunction or catched by thunk
- * @param {*} results from asyncFunction or null if a error is catched by thunk
- * @return {*}
- */
-module.exports = function (thunkable) {
-  // catch the context from mythunk
-  var ctx = this
-  // set the called, the thunk can be called only one time
-  var called
-  // get the arg passed to mythunk, what are the arguments of thunkable
-  var arg = slice.call(arguments,1)
-  // return a thunk
-  return function (cb) {
-    // check if the callback is a function
-    let isFunc = typeof cb === 'function'
-    // check if the thunk was called
-    if(called) return
-    // if is not called then the called is set to true
-    called = true
-    // check if cb is a function
-    // if is a function a callback is pushed to arguments of thunkable
-    if(isFunc) arg.push(function () {
-      // get the arguments passed to this callback
-      var _arg = slice.call(arguments,0)
-      // the callback passed to thunk is called with the arguments getted
-      cb.apply(ctx,_arg)
-    })
-    // if is not a function then is pushed to arg getted to thunkable
-    else if(arguments.length) arg = arg.concat(slice.call(arguments,0))
-    try {
-      // try exec thunkable with the arguments getted
-      return thunkable.apply(ctx,arg)
-    } catch (err) {
-      // if a error is catched then  the cb is called
-      if(isFunc) return cb(err)
-      // if the cb is not a function the error is throw
-      throw err
-    }
-  }
-}
+module.exports = function(thunkable) {
+    const ctx = this;
+    const arg = slice.call(arguments, 1);
+    return function(cb) {
+        const isFunc = typeof cb === 'function';
+        if(isFunc) {
+            arg.push(function() {
+                const _arg = slice.call(arguments, 0);
+                cb.call(ctx, ..._arg);
+            });
+            return thunkable.call(ctx, ...arg);
+        }
+
+        return new Promise((resolve, reject) => {
+            arg.push(function(err) {
+                if (err) return reject(err);
+                const _arg = slice.call(arguments, 1);
+                if (_arg.length === 1) return resolve(_arg[0]);
+                resolve(_arg);
+            });
+            thunkable.call(ctx, ...arg);
+        });
+    };
+};
--- test/test.js
+++ test/test.js
@@ -1,54 +1,70 @@
-'use strict'
-const mythunk = require('../index')
-var assert = require('assert');
+'use strict';
+const mythunk = require('../index');
+const assert = require('assert');
 describe('test to my thunk', () => {
-  before(function () {
-    this.tothunkify = function (arg,cb) {
-      setImmediate(cb,'error',arg)
-      return 'testing'
-    }
-    this.thunk = mythunk(this.tothunkify,null)
-  });
-  it('should return  a function', function () {
-    assert.equal(typeof this.thunk,'function')
-  });
+    before(function() {
+        this.toThunkify = function(arg, cb) {
+            setImmediate(cb, null, arg);
+        };
+        this.thunk = mythunk(this.toThunkify, 'testing');
+    });
+    it('should return  a function', function() {
+        assert.equal(typeof this.thunk, 'function');
+    });
 
-  it('should exec the cb passed ', function (done) {
-    this.thunk(function (err,res) {
-      done()
-    })
-    this.thunk = mythunk(this.tothunkify,null)
-  })
+    it('should exec the cb passed ', function(done) {
+        this.thunk((err, res) => {
+            assert(!err);
+            assert(res === 'testing');
+            done();
+        });
+        this.thunk = mythunk(this.toThunkify, null);
+    });
 
-  it('should exec the cb passed with the error catched in thunkable', function (done) {
-    this.thunk(function (err,res) {
-      assert(err === 'error')
-      done()
-    })
-    this.thunk = mythunk(this.tothunkify, 1)
-  })
+    it('should exec the cb passed with the error catched in thunkable', function(done) {
+        this.thunk((err, res) => {
+            assert(!err);
+            assert(!res);
+            done();
+        });
+        this.thunk = mythunk(this.toThunkify, 1);
+    });
 
+    it('should exec if not callback is not passed', function(done) {
+        this.thunk()
+            .then((res) => {
+                assert(res === 1);
+                done();
+            })
+            .catch(done);
+    });
 
-    it('should exec the cb passed with the the res of thunkable', function (done) {
-      this.thunk(function (err,res) {
-        assert(res === 1)
-        done()
-      })
-      this.thunk = mythunk(this.tothunkify, 1)
-    })
+    it('should pass all arguments', (done) => {
+        const toThunkify = function(arg, cb) {
+            setImmediate(cb, null, 'arg1', 'arg2', 'arg3');
+        };
+        const thunk = mythunk(toThunkify, 'testing');
+        thunk((err, arg1, arg2, arg3) => {
+            assert(!err);
+            assert(arg1 === 'arg1');
+            assert(arg2 === 'arg2');
+            assert(arg3 === 'arg3');
+            done();
+        });
+    });
 
-    it('should return the value returned by thunkable', function () {
-      var res = this.thunk(function () {})
-      assert(res === 'testing')
-      this.thunk = mythunk(function (arg,cb) {
-        throw new Error('testing error')
-      }, 1)
-    })
-
-    it('should catch the error throw by thunkable', function (done) {
-      this.thunk(function (err) {
-        assert(err.message === 'testing error')
-        done()
-      })
-    })
+    it('should pass all arguments', (done) => {
+        const toThunkify = function(arg, cb) {
+            setImmediate(cb, null, 'arg1', 'arg2', 'arg3');
+        };
+        const thunk = mythunk(toThunkify, 'testing');
+        thunk()
+            .then((arg) => {
+                assert(arg[0] === 'arg1');
+                assert(arg[1] === 'arg2');
+                assert(arg[2] === 'arg3');
+                done();
+            })
+            .catch(done);
+    });
 });
